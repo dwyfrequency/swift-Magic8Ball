bdec7e6df2e7750e1fb33b71316bbac84277a467
dwyfrequency <jdwy215@gmail.com>
2019-07-27 18:08:17 -0400

    afunctionality

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Jack Dwyer on 7/27/19.
+//  Copyright © 2019 Jack Dwyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let arr = ["ball1", "ball2","ball3","ball4", "ball5"]
+
+    @IBOutlet weak var EightBallImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateImage()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateImage()
+    }
+    
+    func updateImage() -> Void {
+        let randomNum = Int.random(in: 0 ... 4)
+        EightBallImage.image = UIImage(named: arr[randomNum])
+    }
+
+}
+
+
+
